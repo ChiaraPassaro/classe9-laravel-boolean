c26976db732a0bf34655b2d45b026c6078cda4a9
Chiara Passaro <chiarapassaro.wd@gmail.com>
2020-03-18 08:52:38 +0100

    first milest

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,23 @@
+
+    @include('layouts.partials._header')
+
+    {{-- header --}}
+    <header>
+      @yield('header')
+    </header>
+    {{-- /header --}}
+
+    {{-- main --}}
+    <main>
+        @yield('main')
+    </main>
+    {{-- /main --}}
+
+    {{-- footer --}}
+    <footer>
+       @yield('footer')
+    </footer>
+    {{-- /footer --}}
+    @yield('scripts')
+    
+    @include('layouts.partials._footer')
---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,13 @@
+    <nav class="nav-footer">
+      <ul>
+        <li>
+          <a href="{{route('static_page.privacy')}}">Privacy</a>
+        </li>
+        <li>
+          <a href="{{route('static_page.faq')}}">Domande Frequenti</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</body>
+</html>
